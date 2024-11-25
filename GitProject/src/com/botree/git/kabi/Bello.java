1bc039e9bf8f305b991a324c1fb77c7fcd679249
kabilan <kabilana2002@gmail.com>
2024-11-25 16:04:38 +0530

    naa thaa 2

--- GitProject/src/com/botree/git/kabi/Bello.java
+++ GitProject/src/com/botree/git/kabi/Bello.java
@@ -4,6 +4,7 @@ public class Bello {
 	public static void main(String[] args) {
 		
 		System.out.println("Oora therinjikitan ulagam purinjikitan");
+		System.out.println("ada da mazha da ada mazha da");
 		
 	}
 }
